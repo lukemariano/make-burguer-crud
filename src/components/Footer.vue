24edf3316ad2ba1ccb2cd1275019770622f65723
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-28 19:40:42 -0300

    finalizing project header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+   <footer id="footer">
+        <p>Make Your Burguer &copy; 2022</p>
+   </footer>
 </template>
 
 <script>
@@ -7,3 +9,18 @@ export default {
     name: "Footer",
 }
 </script>
+
+<style scoped>
+    #footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
